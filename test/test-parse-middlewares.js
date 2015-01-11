a0e30633a3068e55013d2f1c1acbdc02a3ff8744
Justin Gallardo <justin.gallardo@gmail.com>
2015-01-11 05:04:43 -0800

    Make Client.parseMiddlewares private.
    Also call middlwares in the context of Client.

--- test/test-parse-middlewares.js
+++ test/test-parse-middlewares.js
@@ -186,3 +186,57 @@ test('stripColor middleware manually added', function(t) {
         mock.close();
     });
 });
+
+test('emit events from parse middleware', function(t) {
+    var client, mock, expected, middlewareHits = 0;
+
+    mock = MockIrcd();
+    client = new irc.Client('localhost', 'testbot', {});
+
+    client.enableParseMiddleware(function(line) {
+        this.emit('parserMiddleware');
+        return line;
+    });
+
+    expected = {
+        sent: [
+            ['NICK testbot', 'Client sent NICK message'],
+            ['USER nodebot 8 * :nodeJS IRC client', 'Client sent USER message'],
+            ['QUIT :node-irc says goodbye', 'Client sent QUIT message']
+        ],
+
+        received: [
+            [':localhost 001 testbot :Welcome to the Internet Relay Chat Network testbot\r\n', 'Received welcome message'],
+            [':jirwin!~jirwin@unaffiliated/jirwin PRIVMSG ##node-irc hello world!\r\n', 'Received hello world message']
+        ]
+    };
+
+    mock.server.on('connection', function() {
+        mock.send(':localhost 001 testbot :Welcome to the Internet Relay Chat Network testbot\r\n');
+    });
+
+    client.on('registered', function() {
+        t.equal(mock.outgoing[0], expected.received[0][0], expected.received[0][1]);
+        mock.send(':jirwin!~jirwin@unaffiliated/jirwin PRIVMSG ##node-irc hello world!\r\n');
+        client.disconnect();
+    });
+
+    client.on('parserMiddleware', function() {
+        middlewareHits++;
+    });
+
+    mock.on('end', function() {
+        var msgs = mock.getIncomingMsgs();
+
+        t.equal(msgs.length, expected.sent.length, 'Sent the same number of messages to the IRC server as we expected');
+        for (var i = 0; i < msgs.length; i++) {
+            t.equal(msgs[i], expected.sent[i][0], expected.sent[i][1]);
+        }
+
+        t.equal(mock.outgoing[1], expected.received[1][0], expected.received[1][1]);
+        t.equal(middlewareHits, expected.received.length, 'For each message received we processed the counter middleware');
+
+        t.end();
+        mock.close();
+    });
+});
